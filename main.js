64c9f7afb98f7fef6851915cc97e6f784e9402bd
OmarBenedettoGianni <omarbenedetto200012@gmail.com>
2026-05-17 16:03:29 +0200

    creato ciclo per tabellina

---
+++ main.js
@@ -0,0 +1,4 @@
+let numeroTabellina = Number(prompt('inserisci il numero per la quale vorresti vedere la tabellina'));
+for (let i = 0; i < 11; i++) {
+    console.log(`Per il numero ${numeroTabellina}, ${numeroTabellina}*${i} è uguale a ${numeroTabellina*i}`);  
+}
